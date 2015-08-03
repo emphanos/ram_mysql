58007b3fb6d8dded49dbd9835cd08c692cf3b10e
Emphanos Tech Support <support@emphanos.com>
2015-08-03 22:22:56 +0000

    make adjustments for mariadb which doesn't use apparmor at all, added tmpdir to ramfs so it doesn't hit the disk at all, still hunting down strange missing API rows not found in DB without any warning messages, need to add final results of tuning for InnoDB into this script

--- make_faster.sh
+++ make_faster.sh
@@ -5,12 +5,13 @@ mount | grep "/var/ramfs" && echo "PROGRESS: looks like /var/ramfs is already mo
 #sudo service mysql-server stop
 sudo /etc/init.d/mysql stop
 sudo cp -R /var/lib/mysql /var/ramfs/
-sudo chown -R mysql:mysql /var/ramfs/mysql
+sudo chown -R mysql:mysql /var/ramfs/
 #in file /etc/mysql/my.cnf
 #Find line with 'datadir' definition(it will look something like datadir = /var/lib/mysql) and change it to
 #datadir = /var/ramfs/mysql
 #sudo cp /etc/mysql/my.cnf /etc/mysql/my.cnf-orig
 sudo sed -i'-orig' 's/datadir.*.=.*/datadir = \/var\/ramfs\/mysql/g' /etc/mysql/my.cnf
+sudo sed -i'-orig' 's/tmpdir.*.=.*/tmpdir = \/var\/ramfs/g' /etc/mysql/my.cnf
 
 #Next step is to tune apparmor settings:
 
@@ -19,19 +20,21 @@ sudo sed -i'-orig' 's/datadir.*.=.*/datadir = \/var\/ramfs\/mysql/g' /etc/mysql/
 #/var/ramfs/mysql/ r,
 #/var/ramfs/mysql/*.pid rw,
 #/var/ramfs/mysql/** rwk,
-echo "  /var/ramfs/mysql/ r,
-  /var/ramfs/mysql/*.pid rw,
-  /var/ramfs/mysql/** rwk," > mod_apparmor_usr.sbin.mysqld
-sudo grep -q '/var/ramfs/mysql' /etc/apparmor.d/usr.sbin.mysqld && echo "PROGRESS: apparmor profile already modified nothing to do here " || sudo sed -i'-orig' '/  \/var\/lib\/mysql\/\*\* rwk,/r mod_apparmor_usr.sbin.mysqld' /etc/apparmor.d/usr.sbin.mysqld
-
+#echo "  /var/ramfs/mysql/ r,
+#  /var/ramfs/mysql/*.pid rw,
+#  /var/ramfs/mysql/** rwk," > mod_apparmor_usr.sbin.mysqld
+#sudo grep -q '/var/ramfs/mysql' /etc/apparmor.d/usr.sbin.mysqld && echo "PROGRESS: apparmor profile already modified nothing to do here " || sudo sed -i'-orig' '/  \/var\/lib\/mysql\/\*\* rwk,/r mod_apparmor_usr.sbin.mysqld' /etc/apparmor.d/usr.sbin.mysqld
+#
 #Looks like we're done with settings, let's see if it will work:
 
-sudo /etc/init.d/apparmor restart
+#sudo /etc/init.d/apparmor restart
 #disable sanity checking using df which will fail for ramfs
 sudo grep -q '#if LC_ALL=C BLOCKSIZE= df' /etc/init.d/mysql && echo "PROGRESS: already using modified mysql init.d script bypassing diskspace check" || sudo cp /etc/init.d/mysql /etc/init.d/dis.mysql; sudo cp init.mysql /etc/init.d/mysql
 sudo /etc/init.d/mysql start
-mysql -e "show variables where Variable_name = 'datadir';" | grep ramfs
+mysql -e "show variables where Variable_name = 'datadir';"
+mysql -e "show variables where Variable_name = 'tmpdir';"
 
 #If mysql daemon starts(double check /var/log/mysql.err for any errors)
-#and you can connect to it, mostlikely now we're running fully off of a RAM device.
+#and you can connect to it, most likely now we're running fully off of a RAM device.
 #To double check it, run this from mysql client:
+#mysql -e "show variables where Variable_name = 'datadir';"
